052554d1ac4bb4f619038397d11133e40efbfd29
Simon Boas <simonboas@gmail.com>
2020-09-27 20:30:49 -0700

    Add removeAll("video")

--- web-pub/main.js
+++ web-pub/main.js
@@ -97,6 +97,7 @@
                 else if( tag == "CLEAR" || tag == "RESTART" ) {
                     removeAll("p");
                     removeAll("img");
+                    removeAll("video");
 
                     // Comment out this line if you want to leave the header visible when clearing
                     setVisible(".header", false);
